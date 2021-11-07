3a3c6e9a2d891b60ab681f366284474f743a86c7
Oguzhan YUKSEL <oguzyuksel92@outlook.com>
2021-11-07 13:05:51 +0300

    Project initializer commit

---
+++ src/Components/TestComponent/TestComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./testcomponent.scss";
+
+function TestComponent() {
+  return (
+    <div>
+      <h1>This is a test component</h1>
+    </div>
+  );
+}
+
+export default TestComponent;
