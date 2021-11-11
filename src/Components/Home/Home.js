c14cade7188d2746d2414630e09d6ab905107d9e
Oguzhan YUKSEL <oguzyuksel92@outlook.com>
2021-11-11 13:57:02 +0300

    Filtering data by input functionality implemented

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,6 +5,7 @@ import axios from "axios";
 function Home() {
   const [countries, setCountries] = useState();
   const [error, setError] = useState();
+  const [searchCountry, setSearchCountry] = useState("");
 
   const getCountries = async () =>
     await axios
@@ -16,16 +17,30 @@ function Home() {
     getCountries();
   }, []);
 
-  error && console.log("ERROR from API:", error);
-  !error && console.log("DATA from API:", countries);
+  // Copy Unique Region Names List for Dropdown List
+  const uniqueRegions = [
+    ...new Set(countries?.map((country) => country.region)),
+  ];
 
+  // Functions
+  const onChangeHandler = (e) => {
+    setSearchCountry(e.target.value);
+  };
+
+  error && console.log("ERROR from API:", error);
+
+  console.log(searchCountry);
   return (
     <div className="homepage-wrapper">
       {/* Top Input Side */}
       <div className="homepage-wrapper-form-area">
         <div className="form-input-area">
           <i className="fas fa-search"></i>
-          <input type="text" placeholder="Search for coutry..." />
+          <input
+            type="text"
+            placeholder="Search for coutry..."
+            onChange={onChangeHandler}
+          />
         </div>
 
         <div>
@@ -33,35 +48,53 @@ function Home() {
             {/* <option value="Filter by Region" selected disabled>
               Filter by Region
             </option> */}
-            {countries?.map((country) => {
-              let uniqueRegions = country.region;
-
-              return <option value={country.region}>{country.region}</option>;
+            {uniqueRegions?.map((region) => {
+              return (
+                <option key={region} value={region}>
+                  {region}
+                </option>
+              );
             })}
           </select>
         </div>
       </div>
       {/* Bottom Country Flag Side */}
       <div className="homepage-wrapper-card-area">
-        {countries?.map((country) => (
-          <div className="homepage-card-item" key={country.name}>
-            <img src={country.flag} alt="test" className="country-flag" />
-            <div className="card-info">
-              <b>{country.name}</b>
-              <small>
-                <b>Pupolation: </b> {country.population.toLocaleString()}
-              </small>
-              <small>
-                <b>Region: </b>
-                {country.region}
-              </small>
-              <small>
-                <b>Capital: </b>
-                {country.capital}
-              </small>
+        {countries
+          // eslint-disable-next-line array-callback-return
+          ?.filter((filteredData) => {
+            if (searchCountry === "") {
+              return filteredData;
+            } else if (
+              filteredData.name
+                .toLowerCase()
+                .includes(searchCountry.toLowerCase())
+            ) {
+              return filteredData;
+            }
+          })
+          .map((country) => (
+            <div
+              className="homepage-card-item"
+              key={`${country.name}${country.area}`}
+            >
+              <img src={country.flag} alt="test" className="country-flag" />
+              <div className="card-info">
+                <b>{country.name}</b>
+                <small>
+                  <b>Pupolation: </b> {country.population.toLocaleString()}
+                </small>
+                <small>
+                  <b>Region: </b>
+                  {country.region}
+                </small>
+                <small>
+                  <b>Capital: </b>
+                  {country.capital}
+                </small>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       {/* TEST */}
     </div>
